b2748cca2e4e9e52053bac78f5c4e29224041302
JacktheGeat <jacklynch706@gmail.com>
2026-05-02 16:36:27 -0400

    made some changes

--- events/views.py
+++ events/views.py
@@ -5,6 +5,9 @@ from .models import Event
 from django.template import loader
 from django.forms.models import model_to_dict
 
+def main(request):
+    return HttpResponse("test")
+
 def show_map(request):
     details = []
     for event in Event.objects.iterator():
@@ -14,5 +17,5 @@ def show_map(request):
               "description": event.description}
         details.append(dd)
     template = loader.get_template("events/listAll.html")
-    context = {"classes": details}
+    context = {"details": details}
     return HttpResponse(template.render(context, request))
